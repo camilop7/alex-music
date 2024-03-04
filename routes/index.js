4c116629ce3ed5f97ac4c5ba49ae45051a50ba66
Camilo Palacios <camilop7@icloud.com>
2024-03-04 19:00:21 +0000

    layout and middleware config

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET bio page. */
+router.get('/bio', function(req, res, next) {
+  res.render('bio', { title: 'Bio' });
+});
+
+/* GET composer page. */
+router.get('/composer', function(req, res, next) {
+  res.render('composer', { title: 'Composer' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+/* GET community page. */
+router.get('/community', function(req, res, next) {
+  res.render('community', { title: 'Community & Events' });
+});
+
 module.exports = router;
